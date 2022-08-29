98dcbf6bf835923c4407347f8ebcf53de9397de5
Konrad <galankonrad@gmail.com>
2022-08-29 13:52:50 +0200

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+
+import { CallToAction } from '@/components/CallToAction'
+import { Faqs } from '@/components/Faqs'
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+import { Hero } from '@/components/Hero'
+import { Pricing } from '@/components/Pricing'
+import { PrimaryFeatures } from '@/components/PrimaryFeatures'
+import { SecondaryFeatures } from '@/components/SecondaryFeatures'
+import { Testimonials } from '@/components/Testimonials'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>VidAsync</title>
+        <meta
+          name="description"
+          content="No more unimportant meetings, free your calendar with the best async communication tool for design & code review."
+        />
+      </Head>
+      <Header />
+      <main>
+        <Hero />
+        <PrimaryFeatures />
+        <SecondaryFeatures />
+        <CallToAction />
+        <Testimonials />
+        <Pricing />
+        <Faqs />
+      </main>
+      <Footer />
+    </>
+  )
+}
